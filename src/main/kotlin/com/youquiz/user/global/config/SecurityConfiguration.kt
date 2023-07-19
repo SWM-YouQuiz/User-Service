3bb740af603868c8359cf0b5efde93b5861dba8e
earlgrey02 <san06036@naver.com>
2023-07-20 00:09:43 +0900

    Feat: SecurityConfiguration JwtAuthenticationFilter 추가

--- src/main/kotlin/com/youquiz/user/global/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/youquiz/user/global/config/SecurityConfiguration.kt
@@ -1,9 +1,12 @@
 package com.youquiz.user.global.config
 
+import com.github.jwt.authentication.JwtAuthenticationFilter
+import com.github.jwt.core.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint
@@ -13,17 +16,14 @@ import org.springframework.security.web.server.context.NoOpServerSecurityContext
 @EnableWebFluxSecurity
 class SecurityConfiguration {
     @Bean
-    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain =
+    fun filterChain(http: ServerHttpSecurity, jwtProvider: JwtProvider): SecurityWebFilterChain =
         with(http) {
             csrf { it.disable() }
             formLogin { it.disable() }
             logout { it.disable() }
             httpBasic { it.authenticationEntryPoint(HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)) }
             securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
-            authorizeExchange {
-                it.anyExchange()
-                    .permitAll()
-            }
+            addFilterAt(JwtAuthenticationFilter(jwtProvider), SecurityWebFiltersOrder.AUTHORIZATION)
             build()
         }
 }
