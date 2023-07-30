4da99a484df035122d8b296e117375554352c4f5
earlgrey02 <san06036@naver.com>
2023-07-30 13:38:50 +0900

    Feat: 데이터베이스를 MySQL에서 MongoDB로 변경

--- src/main/kotlin/com/youquiz/user/domain/User.kt
+++ src/main/kotlin/com/youquiz/user/domain/User.kt
@@ -3,13 +3,13 @@ package com.youquiz.user.domain
 import com.youquiz.user.domain.enum.Role
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.Id
-import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
-@Table
+@Document
 class User(
     @Id
-    var id: Long? = null,
+    var id: String? = null,
     val username: String,
     val password: String,
     val nickname: String,
