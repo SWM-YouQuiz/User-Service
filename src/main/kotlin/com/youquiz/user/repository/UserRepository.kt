1de2574308f33f10df17c208e666fce6473f8b38
earlgrey02 <san06036@naver.com>
2023-07-30 13:57:14 +0900

    Refactor: UserRepository 사용하지 않는 쿼리 메소드 삭제

--- src/main/kotlin/com/youquiz/user/repository/UserRepository.kt
+++ src/main/kotlin/com/youquiz/user/repository/UserRepository.kt
@@ -5,8 +5,6 @@ import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : CoroutineCrudRepository<User, Long> {
-    suspend fun findFirstByUsername(username: String): User?
-
+interface UserRepository : CoroutineCrudRepository<User, String> {
     suspend fun findByUsername(username: String): User?
 }
