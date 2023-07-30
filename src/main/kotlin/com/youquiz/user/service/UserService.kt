64eec4283bc0446677e82d0f0a212dacb2b74cd6
earlgrey02 <san06036@naver.com>
2023-07-30 13:55:13 +0900

    Refactor: UserService createUser 리팩토링

--- src/main/kotlin/com/youquiz/user/service/UserService.kt
+++ src/main/kotlin/com/youquiz/user/service/UserService.kt
@@ -7,6 +7,8 @@ import com.youquiz.user.dto.UserResponse
 import com.youquiz.user.exception.UserNotFoundException
 import com.youquiz.user.exception.UsernameAlreadyExistException
 import com.youquiz.user.repository.UserRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -29,7 +31,11 @@ class UserService(
                     password = passwordEncoder.encode(password),
                     nickname = nickname,
                     role = Role.USER,
-                    allowPush = allowPush
+                    allowPush = allowPush,
+                    answerRate = 0.0,
+                    correctQuizIds = mutableSetOf(),
+                    incorrectQuizIds = mutableSetOf(),
+                    likedQuizIds = mutableSetOf()
                 )
             ).let { UserResponse(it) }
         }
