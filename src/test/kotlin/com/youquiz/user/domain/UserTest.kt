17a21e5a777a4676f59b0eeca68d2a1d064dbb24
earlgrey02 <san06036@naver.com>
2023-08-05 00:55:12 +0900

    Test: User 엔티티 likeQuiz 및 unlikeQuiz 테스트

--- src/test/kotlin/com/youquiz/user/domain/UserTest.kt
+++ src/test/kotlin/com/youquiz/user/domain/UserTest.kt
@@ -5,13 +5,15 @@ import com.youquiz.user.fixture.createUser
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.doubles.shouldBeGreaterThan
 import io.kotest.matchers.doubles.shouldBeLessThan
+import io.kotest.matchers.ints.shouldBeGreaterThan
+import io.kotest.matchers.ints.shouldBeLessThan
 
 class UserTest : BehaviorSpec() {
     init {
         Given("유저가 존재하는 경우") {
             val user = createUser()
 
-            When("유저가 해당 퀴즈의 정답을 맞췄다면") {
+            When("유저가 퀴즈의 정답을 맞췄다면") {
                 val increasedUser = createUser().apply { correctAnswer(ID) }
 
                 Then("해당 유저의 정답률이 상승한다.") {
@@ -19,11 +21,27 @@ class UserTest : BehaviorSpec() {
                 }
             }
 
-            When("유저가 해당 퀴즈의 정답을 틀렸다면") {
+            When("유저가 퀴즈의 정답을 틀렸다면") {
                 val decreasedUser = createUser().apply { incorrectAnswer(ID) }
 
                 Then("해당 유저의 정답률이 감소한다.") {
                     decreasedUser.answerRate shouldBeLessThan user.answerRate
+                }
+            }
+
+            When("유저가 퀴즈에 좋아요를 했다면") {
+                val likeUser = createUser().apply { likeQuiz(ID) }
+
+                Then("해당 유저의 좋아요한 퀴즈가 추가된다.") {
+                    likeUser.likedQuizIds.size shouldBeGreaterThan user.likedQuizIds.size
+                }
+            }
+
+            When("유저가 퀴즈에 좋아요 취소를 했다면") {
+                val unlikeUser = createUser().apply { unlikeQuiz(likedQuizIds.random()) }
+
+                Then("해당 유저의 좋아요한 퀴즈가 삭제된다.") {
+                    unlikeUser.likedQuizIds.size shouldBeLessThan user.likedQuizIds.size
                 }
             }
         }
