ef499f1e52d1537d400b31d632329aeb85449f95
earlgrey02 <san06036@naver.com>
2023-08-28 21:36:00 +0900

    Feat: UserRepository findAllOrderByCorrectQuizIdsSize 구현

--- src/main/kotlin/com/quizit/user/repository/UserRepository.kt
+++ src/main/kotlin/com/quizit/user/repository/UserRepository.kt
@@ -1,10 +1,20 @@
 package com.quizit.user.repository
 
 import com.quizit.user.domain.User
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.mongodb.repository.Aggregation
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository : CoroutineCrudRepository<User, String> {
     suspend fun findByUsername(username: String): User?
+
+    @Aggregation(
+        pipeline = [
+            "{ \$addFields: { quizCount: { \$size: '\$correctQuizIds' } }}",
+            "{ \$sort: { quizCount: -1 } }"
+        ]
+    )
+    fun findAllOrderByCorrectQuizIdsSize(): Flow<User>
 }
