7870bd0440127a66563765b80f5b434721e383b0
earlgrey02 <san06036@naver.com>
2023-09-03 13:52:09 +0900

    Feat: 권한 제어에서 누락된 엔드포인트 추가

--- src/main/kotlin/com/quizit/user/global/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/quizit/user/global/config/SecurityConfiguration.kt
@@ -36,6 +36,7 @@ class SecurityConfiguration {
                 it.pathMatchers("/user/admin/**")
                     .hasAuthority("ADMIN")
                     .pathMatchers(
+                        "/user",
                         "/user/username/{username}",
                         "/user/username/{username}/match-password"
                     )
