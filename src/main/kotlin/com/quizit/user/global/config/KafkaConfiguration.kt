d4686ac762bff147218b44286d5dc2cacb5ef06c
earlgrey02 <san06036@naver.com>
2023-10-21 20:52:14 +0900

    Feat: KafkaConfiguration producer 및 consumer 설정 추가

--- src/main/kotlin/com/quizit/user/global/config/KafkaConfiguration.kt
+++ src/main/kotlin/com/quizit/user/global/config/KafkaConfiguration.kt
@@ -1,6 +1,7 @@
 package com.quizit.user.global.config
 
 import com.quizit.user.dto.event.CheckAnswerEvent
+import com.quizit.user.dto.event.DeleteQuizEvent
 import com.quizit.user.dto.event.MarkQuizEvent
 import com.quizit.user.global.util.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
@@ -8,21 +9,26 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
 import reactor.kafka.receiver.ReceiverOptions
+import reactor.kafka.sender.SenderOptions
 
 @Configuration
 class KafkaConfiguration(
     private val properties: KafkaProperties,
 ) {
-    private inline fun <reified T> receiverOptions(topic: String): ReceiverOptions<String, T> =
-        ReceiverOptions.create<String, T>(
-            properties.run {
-                consumer.groupId = "$topic-group"
-                buildConsumerProperties()
-            })
-            .subscription(listOf(topic))
-            .withValueDeserializer(JsonDeserializer<T>(T::class.java, false))
+    @Bean
+    fun reactiveKafkaProducerTemplate(properties: KafkaProperties): ReactiveKafkaProducerTemplate<String, Any> =
+        ReactiveKafkaProducerTemplate(
+            SenderOptions.create<String, Any>(properties.buildProducerProperties())
+                .withValueSerializer(JsonSerializer())
+        )
+
+    @Bean
+    fun deleteQuizConsumer(): ReactiveKafkaConsumerTemplate<String, DeleteQuizEvent> =
+        ReactiveKafkaConsumerTemplate(receiverOptions("delete-quiz"))
 
     @Bean
     fun markQuizConsumer(): ReactiveKafkaConsumerTemplate<String, MarkQuizEvent> =
@@ -31,6 +37,19 @@ class KafkaConfiguration(
     @Bean
     fun checkAnswerConsumer(): ReactiveKafkaConsumerTemplate<String, CheckAnswerEvent> =
         ReactiveKafkaConsumerTemplate(receiverOptions("check-answer"))
+
+    private inline fun <reified T> receiverOptions(topic: String): ReceiverOptions<String, T> =
+        ReceiverOptions.create<String, T>(
+            properties.run {
+                consumer.groupId = "$topic-group"
+                buildConsumerProperties()
+            })
+            .subscription(listOf(topic))
+            .withValueDeserializer(JsonDeserializer<T>(T::class.java, false))
+}
+
+fun producerLogging(event: Any) {
+    logger.info { "Successfully produced ${event::class.simpleName}: $event" }
 }
 
 fun consumerLogging(consumerRecord: ConsumerRecord<String, out Any>) {
