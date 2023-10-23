e7381b8ad7960840602b9b0ed981fece28df1775
earlgrey02 <san06036@naver.com>
2023-10-23 23:54:29 +0900

    Style: 파라미터명 변경

--- src/main/kotlin/com/quizit/user/global/util/WebUtil.kt
+++ src/main/kotlin/com/quizit/user/global/util/WebUtil.kt
@@ -10,8 +10,8 @@ import org.springframework.web.reactive.function.server.queryParamOrNull
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.cast
 
-fun RouterFunctionDsl.queryParams(vararg name: String): RequestPredicate =
-    name.map { queryParam(it) { true } }.reduce { total, next -> total and next }
+fun RouterFunctionDsl.queryParams(vararg names: String): RequestPredicate =
+    names.map { queryParam(it) { true } }.reduce { total, next -> total and next }
 
 inline fun <reified T> ServerRequest.queryParamNotNull(name: String): T =
     this.queryParamOrNull(name)!!.run {
